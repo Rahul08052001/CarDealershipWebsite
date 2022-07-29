6ba8c2686f80bb8e709fbb667a9ddc6a791d3c1f
Rahul08052001 <110226704+Rahul08052001@users.noreply.github.com>
2022-07-29 15:44:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // }
 
 var img1 = document.getElementById('gwagon');
-    var imgarr1 = ['./Media/gwagon2.jpg','./Media/gwagon3.jpg','./Media/gwagon4.jpg','./Media/gwagon5.jpg'];
+    var imgarr1 = ['gwagon2.jpg','gwagon3.jpg','gwagon4.jpg','gwagon5.jpg'];
     var i = 0;
     function slide(){
         img1.setAttribute('src',imgarr1[i]);
@@ -17,7 +17,7 @@ var img1 = document.getElementById('gwagon');
 setInterval('slide()',2500);   
 
 var img2 = document.getElementById('porshe');
-    var imgarr2 = ['./Media/Porshe2.jpeg','./Media/Porshe3.jpeg','./Media/Porshe4.jpeg'];
+    var imgarr2 = ['Porshe2.jpeg','Porshe3.jpeg','Porshe4.jpeg'];
     var j = 0;
     function slide1(){
         img2.setAttribute('src',imgarr2[j]);
@@ -29,7 +29,7 @@ var img2 = document.getElementById('porshe');
 setInterval('slide1()',2500);   
 
 var img3 = document.getElementById('mercedes');
-    var imgarr3 = ['./Media/maybach2.jpg','./Media/maybach3.jpg','./Media/maybach4.jpg','./Media/maybach5.jpg'];
+    var imgarr3 = ['maybach2.jpg','maybach3.jpg','maybach4.jpg','maybach5.jpg'];
     var k = 0;
     function slide2(){
         img3.setAttribute('src',imgarr3[k]);
@@ -41,7 +41,7 @@ var img3 = document.getElementById('mercedes');
 setInterval('slide2()',2500);   
 
 var img4 = document.getElementById('mclaren');
-    var imgarr4 = ['./Media/mclaren1.jpg','./Media/mclaren3.jpg','./Media/mclaren4.jpg','./Media/mclaren5.jpg'];
+    var imgarr4 = ['mclaren1.jpg','mclaren3.jpg','mclaren4.jpg','mclaren5.jpg'];
     var l = 0;
     function slide3(){
         img4.setAttribute('src',imgarr4[l]);
@@ -53,7 +53,7 @@ var img4 = document.getElementById('mclaren');
 setInterval('slide3()',2500);   
 
 var img5 = document.getElementById('RR');
-    var imgarr5 = ['./Media/RR2.jpg','./Media/RR3.jpg','./Media/RR4.jpg','./Media/RR5.jpg'];
+    var imgarr5 = ['RR2.jpg','RR3.jpg','RR4.jpg','RR5.jpg'];
     var a = 0;
     function slide4(){
         img5.setAttribute('src',imgarr5[a]);
@@ -65,7 +65,7 @@ var img5 = document.getElementById('RR');
 setInterval('slide4()',2500);   
 
 var img6 = document.getElementById('meserati');
-    var imgarr6 = ['./Media/meserati2.jpg','./Media/mesarati3.jpg','./Media/mesarati4.jpg','./Media/mesarati5.jpg'];
+    var imgarr6 = ['meserati2.jpg','mesarati3.jpg','mesarati4.jpg','mesarati5.jpg'];
     var b = 0;
     function slide5(){
         img6.setAttribute('src',imgarr6[b]);
